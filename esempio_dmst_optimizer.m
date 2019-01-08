e3f18bd400cdc9f70f2496d41cb21a176da57940
RaydenX93 <delucastefano@hotmail.com>
2019-01-08 15:33:34 +0100

    Working example

--- esempio_dmst_optimizer.m
+++ esempio_dmst_optimizer.m
@@ -2,9 +2,14 @@ clear all
 close all
 clc
 
-[x,fval,exitflag,output] = fminbnd(vatt_dmst_optim,x1,x2);
+depth = [0 -15];
+vel_x = [1.75 1.75];
+vel_input = [depth', vel_x'];
 
- function cp = vatt_dmst_optim(x)
+fun = @(x) vatt_dmst_optim(vel_input,x);
+[tsr_opt,fval,exitflag,output] = fminbnd(fun,2,3);
+
+ function cp = vatt_dmst_optim(vel_input, x)
     [data_post, ~, ~, ~, ~, ~, ~, ~] = vatt_dmst(vel_input, 'simulazione3d_optim', x);
-    cp = data_post.cp_tot;
+    cp = - data_post.cp_tot;
  end
