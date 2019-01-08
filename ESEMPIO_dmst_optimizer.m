b68ed71c959154f21330eabfc7715a901b39892e
RaydenX93 <delucastefano@hotmail.com>
2019-01-08 18:07:12 +0100

    More intuitive files

--- ESEMPIO_dmst_optimizer.m
+++ ESEMPIO_dmst_optimizer.m
@@ -6,8 +6,15 @@ depth = [0 -15];
 vel_x = [1.75 1.75];
 vel_input = [depth', vel_x'];
 
+min_tsr = 2;
+max_tsr = 3;
+
+%% Identifica TSR ottimale per simulazione 3D %%
 fun = @(x) vatt_dmst_optim(vel_input,x);
-[tsr_opt,fval,exitflag,output] = fminbnd(fun,2,3);
+[tsr_opt,fval,exitflag,output] = fminbnd(fun,min_tsr,max_tsr); 
+
+tsr_opt
+fval = - fval
 
  function cp = vatt_dmst_optim(vel_input, x)
     [data_post, ~, ~, ~, ~, ~, ~, ~] = vatt_dmst(vel_input, 'simulazione3d_optim', x);
